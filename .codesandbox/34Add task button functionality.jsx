e70cc915d9c120ad4ec32c81e053987b8c6d72ee
yuya <65487059+jojo232@users.noreply.github.com>
2023-03-26 22:39:20 +0900

    Update 34Add task button functionality.jsx

--- .codesandbox/34Add task button functionality.jsx
+++ .codesandbox/34Add task button functionality.jsx
@@ -57,6 +57,55 @@ document
   const deleteTarget = deleteButton.parentNode;
   document.getElementById("incomplete-list").removeChild(deleteTarget);
 
+
+---------------------------------------------------------------------------
+import "./styles.css";
+
+const onClickAdd = () => {
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+  //テクストボックスの値を取得し、初期化する。
+
+  //dev生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  //liタグ生成
+  const li = document.createElement("li");
+  li.innerText = inputText;
+
+  // button(完了)タグの生成
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => {
+    alert("完了");
+  });
+
+  //button(削除)タグの生成
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+
+    // 押された削除ボタンの親タグ(div)を未完了リストから削除
+    const deleteTarget = deleteButton.parentNode;
+    document.getElementById("incomplete-list").removeChild(deleteTarget);
+  });
+
+  //divタグの子要素に各要素を設定する
+  div.appendChild(li);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
+
+  // 未完了リストに追加
+  document.getElementById("incomplete-list").appendChild(div);
+};
+
+//buttonクリック処理
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
+---------------------------------------------------------------------------
+
 ------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 // 押された完了ボタンの親タグ(div)を未完了リストから削除
